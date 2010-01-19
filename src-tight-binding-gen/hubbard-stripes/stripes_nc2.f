bfd78a33c553e5e75083771b5a5ad8dd55867a0a
deisz <deisz@29e302fd-eee4-0310-9350-c7e3c18e3f00>
2010-01-19 22:20:04 +0000

    added n=2 stripes input file

---
+++ src-tight-binding-gen/hubbard-stripes/stripes_nc2.f
@@ -0,0 +1,76 @@
+      program oneband
+
+c By assumption the unit cell contains n atoms along the x-direction
+c and 1 atom along the y-direction
+  
+      implicit none
+
+      DOUBLE PRECISION t(0:1,0:1,-2:2,-2:2), ed(0:1), flux(1:3)
+
+      integer ix, iy, i1, i2
+      DOUBLE PRECISION tx, ty, txy, txx, tyy
+      
+      t  = 0.0d0
+
+      read(5,*) flux(1), flux(2), flux(3)
+      read(5,*) ed(0), ed(1)
+      read(5,*) tx
+      read(5,*) ty
+      read(5,*) txy
+      read(5,*) txx
+      read(5,*) tyy
+
+      t(0,0,1,0) = -txx
+      t(0,0,-1,0) = -txx
+      t(0,0,0,1) = -ty
+      t(0,0,0,-1) = -ty
+      t(0,0,1,1) = 0.0d0
+      t(0,0,1,-1) = 0.d0
+      t(0,0,-1,1) = 0.0d0
+      t(0,0,-1,-1) = 0.0d0
+
+      t(1,1,1,0) = -txx
+      t(1,1,-1,0) = -txx
+      t(1,1,0,1) = -ty
+      t(1,1,0,-1) = -ty
+      t(1,1,1,1) = -t3
+      t(1,1,1,-1) = -t3
+      t(1,1,-1,1) = -t3
+      t(1,1,-1,-1) = -t3
+
+      t(0,1,1,1) = t4
+      t(0,1,1,-1) = -t4
+      t(0,1,-1,1) = -t4
+      t(0,1,-1,-1) = t4
+
+      t(1,0,1,1) = t4
+      t(1,0,1,-1) = -t4
+      t(1,0,-1,1) = -t4
+      t(1,0,-1,-1) = t4
+
+      write(6,*) '--------------flux values --------------'
+      write(6,*) flux(1), flux(2), flux(3)
+      write(6,*) 
+      write(6,*) '--------------orbital level-------------'
+      write(6,*) 0, ed(0)
+      write(6,*) 1, ed(1)
+
+      do ix = -2,2
+        do iy = -2,2
+          write(6,*) 
+          write(6,200) ix,iy
+          do i1 = 0, 1
+           do i2 = 0, 1 
+          write(6,300) i1,i2,t(i1,i2,ix,iy),0.0d0
+          enddo
+          enddo
+        enddo
+      enddo
+
+ 200  format('------------------- [',i3,',',i3,'] hopping',
+     $   '------------------------')
+ 300  format(i3,',',i3,'  ','(',D16.9,',',D16.9,')')
+
+
+      stop
+      end
